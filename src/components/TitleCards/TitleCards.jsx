1dadb613f71a7935751ba1980a192b7354e24b81
pinto10g <princetonpinto94@gmail.com>
2024-11-08 08:39:54 +0000

    Use react-router-dom and API to fetch movies list

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([])
+
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ${process.env.API_TOKEN}'
+    }
+  };
+  
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -13,6 +24,12 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel',handleWheel);
   }, [])
 
@@ -20,10 +37,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
-                    <img src={card.image} alt="card-image" />
-                    <p>{card.name}</p>
+                    <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="card-image" />
+                    <p>{card.original_title}</p>
                  </div>
         })}
       </div>
